f33ec29be7a1fa69a2c2ffc414d7df53c62a185b
smajaas <smajaas@gmail.com>
2022-11-09 20:10:35 +0530

    http Ajax done

---
+++ Simple text,btn/app.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+  getData();
+});
+
+function getData() {
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', './api/sample.txt');
+  xhr.onreadystatechange = function () {
+    //console.log(xhr);
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const text = document.createElement('p');
+      text.textContent = xhr.responseText;
+      document.body.appendChild(text);
+    } else {
+      console.log({
+        status: xhr.status,
+        text: xhr.statusText,
+        state: this.readyState,
+      });
+    }
+  };
+  xhr.send();
+}
